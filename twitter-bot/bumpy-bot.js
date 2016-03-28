3d468d7b643d80f8e50af8dc64dd2744836e6b1f
Westley Hennigh-Palermo <westleyargentum@gmail.com>
2016-03-28 15:35:39 -0700

    fist pass at bot

--- twitter-bot/bumpy-bot.js
+++ twitter-bot/bumpy-bot.js
@@ -1,6 +1,36 @@
-var Twit = require('twit'),
-    particleConfig = require('particle.config'),
-    twitterConfig = require('twitter.config');
+var Particle = require('particle-api-js'),
+    Twit = require('twit'),
+    config = require('./config.json'),
+    particleConfig = require('./particle-config.json'),
+    twitterConfig = require('./twitter-config.json');
 
+var particle = new Particle();
 var twit = new Twit(twitterConfig);
 
+// -------
+
+particle.login(particleConfig).then(
+    function(data) {
+        console.log('connected to particle');
+        var token = data.body.access_token;
+
+        particle.getEventStream({ deviceId: config.deviceId, auth: token }).then(function(stream) {
+            stream.on('event', function(event) {
+                console.log("event: " + event.name, ' ', event.published_at, '\n', event.data);
+
+                if (config.reallyActuallyTweet) {
+                    twit.post('statuses/update', { status: 'pothole pothole pothole, bump bump bump!' }, function(err, data, response) {
+                        if (err) {
+                            console.log("ERROR TWEETING:", err);
+                        }
+                        console.log('tweeted:\n', data.id_str, '\n', data.text);
+                    });
+                }
+            });
+        });
+    },
+
+    function(err) {
+        console.log('ERROR: FAILED TO LOGIN\n', err);
+    }
+);
